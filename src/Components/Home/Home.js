5e582f9d1a6dfd7177e385fecb6622a45e0e0505
darling1823 <gabrielalcantara1823@gmail.com>
2022-09-11 13:08:41 -0300

    home update

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,16 +1,40 @@
 import React from 'react'
 import Background from '../data/homebg.jpg'
+import Dog from '../data/dog.png'
+import Cat from '../data/cat.png'
+import Help from '../data/help.png'
 import * as S from './Style'
 
 const Home = () => {
  return(
-    <div>
-        <S.HOMEBG>
+    <S.HomeContainer>
+        <S.HomeBG>
             <img src={Background} alt='Background'/>
-        </S.HOMEBG>
-        
-
-    </div>
+        </S.HomeBG>
+        <S.Container>
+            <S.H2>Quem somos?</S.H2>
+            <S.HomeDiv>
+                <S.HomeFig>
+                    <S.IMG src={Dog} alt='Dog'/>
+                </S.HomeFig>
+                <S.P>Nós somos uma ONG focada em ajudar amigos peludos que estão perdidos por todo o país. Aqui conseguem conforto e carinho!</S.P>
+            </S.HomeDiv>
+            <S.H2>O que fazemos?</S.H2>
+            <S.HomeDiv>
+                <S.P>Abrigamos o máximo de amigos possíveis em nossos abrigos até que possam encontram uma nova casa!</S.P>
+                <S.HomeFig>
+                    <S.CAT src={Cat} alt='Cat'/>
+                </S.HomeFig>
+            </S.HomeDiv>
+            <S.H2>Como você pode ajudar?</S.H2>
+            <S.HomeDiv>
+                <S.FigHelp>
+                    <S.IMG src={Help} alt='Help'/>
+                </S.FigHelp>
+                <S.P>Além de fazer doações nos centros de abrigo, você também pode adotar um amigo através desse site!</S.P>
+            </S.HomeDiv>
+        </S.Container>
+    </S.HomeContainer>
  )
 }
 
--- src/Components/Home/Style.js
+++ src/Components/Home/Style.js
@@ -2,7 +2,6 @@ import styled, {createGlobalStyle} from 'styled-components'
 import {Link} from 'react-router-dom'
 
 export const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,200;0,300;0,400;1,300;1,400&family=Lobster&display=swap');
     *{
         margin: 0;
         padding: 0;
@@ -18,7 +17,6 @@ export const DIV = styled.div`
     align-items: center;
     justify-content: center;
     height: 200px;
-    background-color: #F7B538;
     position: relative;
     z-index: 0;
     overflow: hidden;
@@ -38,13 +36,11 @@ export const DIV = styled.div`
 `
 export const MID = styled.img`
     position: absolute;
-    opacity: 0.5;
     z-index: -1;
     width: 45vw;
 `
 export const LEFT = styled.img`
     position: absolute;
-    opacity: 0.5;
     left: -17.5vw;
     z-index: -1;
     width: 45vw;
@@ -52,7 +48,6 @@ export const LEFT = styled.img`
 
 export const RIGHT = styled.img`
     position: absolute;
-    opacity: 0.5;
     right: -17.5vw;
     z-index: -1;
     width: 45vw;
@@ -60,8 +55,8 @@ export const RIGHT = styled.img`
 
 export const H1 = styled.h1`
     z-index: 1;
-    font-family: 'Lobster', cursive;
-    color: #00000;
+    font-family: 'Pacifico', cursive;
+    color: #780116;
     font-size: 5vw;
     @media screen and (max-width: 1024px){
         font-size: 6vw;
@@ -82,7 +77,7 @@ export const UL = styled.ul`
     position: absolute;
     right: 0;
     bottom: 0;
-    opacity: 0.9;
+   
     display: flex;
     justify-content: space-evenly;
     align-items: center;
@@ -154,10 +149,182 @@ export const ULMOB = styled.ul`
     align-items: center;
 `
 
-export const HOMEBG = styled.figure`
+export const HomeBG = styled.figure`
     overflow: hidden;
-    width: 99vw;
+    width: 100vw;
     display: flex;
     justify-content: center;
     align-items: center;
+    position: absolute;
+    z-index: 0;
+    @media screen and (max-width: 768px){
+        display: none;
+    }
+`
+
+export const HomeContainer = styled.div`
+    position: relative;
+    display: flex;
+    justify-content: center;
+`
+
+export const Container = styled.div`
+    z-index: 1;
+    margin-top: 2vw;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 75%;
+    background: #F7B538;
+    border: solid 5px #780116;
+    border-radius: 10px;
+    margin-bottom: 2vw;
+    @media screen and (max-width: 768px){
+        width: 100%;
+        border-radius: 0px;
+        background: none;
+        overflow: hidden;
+        border: none;
+        border-bottom: solid 5px #780116;
+    }
+`
+
+export const H2 = styled.h2`
+    color: #D8572A;
+    background: #780116;
+    text-align: center;
+    width: 100%;
+    font-size: 3vw;
+    font-family: 'Satisfy', cursive;
+    @media screen and (max-width: 1024px){
+        font-size: 3.5vw;
+    }
+    @media screen and (max-width: 768px){
+        font-size: 4vw;
+    }
+    @media screen and (max-width: 425px){
+        font-size: 5.5vw;
+    }
+    @media screen and (max-width: 320px){
+        font-size: 6.5vw;
+    }
+`
+
+export const HomeDiv = styled.div`
+    display: flex;
+    align-items: center;
+    padding-top: 1vw;
+    padding-bottom: 1vw;
+    height: 400px;
+    position: relative;
+    @media screen and (max-width: 1024px){
+        height: 300px;
+    }
+    @media screen and (max-width: 425px){
+        height: 200px;
+    }
+    @media screen and (max-width: 320px){
+        height: 150px;
+    }
+    
+`
+
+export const HomeFig = styled.figure`
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 20vw;
+    @media screen and (max-width: 768px){
+        position: absolute;
+        width: 100vw;
+        height: 300px;
+    }
+    @media screen and (max-width: 425px){
+        height: 200px;
+    }
+    @media screen and (max-width: 320px){
+        height: 150px;
+    }
+`
+
+export const IMG = styled.img`
+    width: 400px;
+    @media screen and (max-width: 1024px){
+        width: 340px;
+    }
+    @media screen and (max-width: 768px){
+        width: 800px;
+        opacity: 0.3;
+    }
+    @media screen and (max-width: 425px){
+        width: 500px;
+    }
+    @media screen and (max-width: 320px){
+        width: 420px;
+    }
+`
+
+export const P = styled.p`
+    color: #DB7C26;
+    font-size: 2vw;
+    width: 45vw;
+    @media screen and (max-width: 1024px){
+        font-size: 2.5vw;
+    }
+    @media screen and (max-width: 768px){
+        font-size: 3.5vw;
+        width: 60vw;
+        color: #780116;
+    }
+    @media screen and (max-width: 425px){
+        font-size: 5vw;
+        width: 70vw;
+    }
+    @media screen and (max-width: 320px){
+        font-size: 5.5vw;
+        width: 75vw;
+    }
+`
+
+export const CAT = styled.img`
+    width: 300px;
+    @media screen and (max-width: 1440px){
+        width: 290px;
+    }
+    @media screen and (max-width: 1024px){
+        width: 200px;
+    }
+    @media screen and (max-width: 768px){
+        width: 550px;
+        opacity: 0.3;
+    }
+    @media screen and (max-width: 425px){
+        width: 350px;
+    }
+    @media screen and (max-width: 320px){
+        width: 290px;
+    }
+`
+
+export const FigHelp = styled.figure`
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 25vw;
+    @media screen and (max-width: 1024px){
+        width: 30vw;
+    }
+    @media screen and (max-width: 768px){
+        position: absolute;
+        width: 100vw;
+        height: 300px;
+    }
+    @media screen and (max-width: 425px){
+        height: 200px;
+    }
+    @media screen and (max-width: 320px){
+        height: 150px;
+    }
 `
